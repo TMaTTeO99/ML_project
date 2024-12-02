06de662bcf18648f238fb5919ad0e58ef77b992f
Davide Rizzello <davide.rizzello.sc@gmail.com>
2024-12-02 18:03:21 +0100

    test feedforward

--- model.py
+++ model.py
@@ -25,8 +25,9 @@ class NeuralNetwork():
             weightMatricesList.append(self.random_matrix(self.units_for_levels[i], self.units_for_levels[i+1]))
         return weightMatricesList
 
-    def print_matrices_fancy(self):
-        for idx, matrix in enumerate(self.listOfWeightMatrices):
+    @staticmethod
+    def print_matrices_fancy(list_to_print):
+        for idx, matrix in enumerate(list_to_print):
             print(f"Matrix {idx + 1}:")
             for row in matrix:
                 formatted_row = " | ".join(f"{elem:6.2f}" for elem in row)
@@ -55,19 +56,27 @@ class NeuralNetwork():
     def feedForeward (self, inputX):
 
         localInputX = inputX
+        self.listOfHiddenRepr = []
         #level means W matrix for all levels
         for level in range(len(self.listOfWeightMatrices)):
 
-            localInputX = np.mathmul(localInputX, self.listOfWeightMatrices[level])
+            localInputX = np.matmul(localInputX, self.listOfWeightMatrices[level])
             localInputX = self.activation(localInputX)
-        
+            self.listOfHiddenRepr.append(localInputX)
+
+            # print for debug
+            print("Stampa rappresentazione interna")
+            self.print_matrices_fancy(self.listOfHiddenRepr)
+
         # localInputX == output of feedForeward of the NN  
         return localInputX; 
             
 
+    def predict(self, inputX):
+        return self.feedForeward(inputX)
 
 
 
-
-
+    def get_list_weight_matrices(self):
+        return self.listOfWeightMatrices
 
