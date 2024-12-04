b057d6e4f33d0d0ee6a017d1aa02fd0e9e1ec841
Matteo <teo@MattePc.>
2024-12-04 22:44:35 +0100

    fixed transpose bugs and started training function

--- train.py
+++ train.py
@@ -2,16 +2,21 @@ from model import NeuralNetwork
 import numpy as np
 
 # instantiate the neural network
-model = NeuralNetwork([3,2,1], ['sigmoid','linear'])
+model = NeuralNetwork([2,2,1], ['sigmoid','sigmoid'])
 
 print("Matrice dei pesi: ")
 model.print_matrices_fancy(model.get_list_weight_matrices())
 
     
-matrix = np.array([[1, 2, 3]]) 
+matrix = np.array([ [1, 0],
+                [1, 1],
+                [0, 0],
+                [0, 1] ]) 
 #output = model.predict(matrix)
 # print("Stampa finale")
 # print(output)
 
-model.train(matrix, [5])
+model.train(matrix, np.array([[1], [0], [0], [1]]))
 
+result = model.predict(np.array(  [[0, 0]] ))
+print(f" predizione 0, 1 : {result}")
